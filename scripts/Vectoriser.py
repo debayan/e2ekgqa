c8f991530abf53a953963e9ad4869b62c5275780
Debayan Banerjee <debayanin@gmail.com>
2021-12-05 20:05:06 +0100

    candidates vectorised

---
+++ scripts/Vectoriser.py
@@ -0,0 +1,127 @@
+import logging,os
+import json
+import sys
+import requests
+import torch.multiprocessing
+from elasticsearch import Elasticsearch
+from sentence_transformers import SentenceTransformer, util
+
+
+sentencemodel = SentenceTransformer('all-MiniLM-L6-v2')
+
+reldict = json.loads(open('en.json').read())
+goldrellabels = []
+
+for k,v in reldict.items():
+    goldrellabels.append([k,v])
+
+sentence_embeddings = sentencemodel.encode([x[1] for x in goldrellabels], convert_to_tensor=True)
+
+es = Elasticsearch(host='ltcpu1',port=49158)
+
+
+def ft(labels): #fasttext
+    r = requests.post("http://ltcpu1:49157/ftwv",json={'chunks': labels},headers={'Connection':'close'})
+    descembedding = r.json()
+    return descembedding
+
+def kgembed(entid): #fasttext
+    enturl = '<http://www.wikidata.org/entity/'+entid+'>'
+    res = es.search(index="wikidataembedsindex01", body={"query":{"term":{"key":{"value":enturl}}}})
+    try:
+        embedding = [float(x) for x in res['hits']['hits'][0]['_source']['embedding']]
+        return embedding
+    except Exception as e:
+        print(entid,' entity embedding not found')
+        return 200*[0.0]
+
+def relcands(rellabel):
+    results = []
+    query = rellabel.strip()
+    if not query:
+        return []
+    query_embedding = sentencemodel.encode(query, convert_to_tensor=True)
+
+    # We use cosine-similarity and torch.topk to find the highest 5 scores
+    cos_scores = util.pytorch_cos_sim(query_embedding, sentence_embeddings)[0]
+    top_results = torch.topk(cos_scores, k=30)
+
+    #print("\n\n======================\n\n")
+    #print("Query:", query)
+    #print("\nTop 5 most similar sentences in corpus:")
+
+    for score, idx in zip(top_results[0], top_results[1]):
+        #print(goldrellabels[idx], "(Score: {:.4f})".format(score))
+        results.append(goldrellabels[idx])
+    return results
+
+def entcands(entlabel):
+    esresults = es.search(index='wikidataentitylabelindex02',body={"query":{"match":{"wikidataLabel":entlabel}}},size=30)
+    results = []
+    try:
+        for res in esresults['hits']['hits']:
+            #print(entlabel, res['_source'])
+            results.append(res['_source'])
+        return results
+    except Exception as err:
+        print(entlabel,err)
+        return results
+
+
+
+def vectorise(question,labels):
+    entss = ''
+    relss = ''
+    try:
+        entss,relss = labels.split('//')
+    except Exception as err:
+        print(err, labels)
+        entss = labels
+    try:
+        ents = entss.split('::')
+        rels = relss.split(';;')
+    except Exception as err:
+        print("error:",err)
+        return [],[]
+    print("labels:",labels)
+    print("ents      :",ents)
+    print("rels      :",rels)
+    entlabelcands = {}
+    rellabelcands = {}
+    for ent in ents:
+        entlabelcands[ent] =  entcands(ent)
+    for rel in rels:
+        rellabelcands[rel] =  relcands(rel)
+    strarr = []
+    embarr = []
+    questionarr = question.split()
+    questionftembed = ft(questionarr)
+    strarr = questionarr
+    embarr = [x+200*[0.0] for x in questionftembed]
+    #[print(x,y) for x,y in zip(questionarr,questionftembed)]
+    strarr += ['[SEP]']
+    embarr += [500*[-1.0]]
+    for k,v in entlabelcands.items():
+        groundlabel = k
+        groundembed = ft(groundlabel)
+        labelembeds = ft([x['wikidataLabel'] for x in v])
+        entembs = [kgembed(x['uri']) for x in v]
+        #[print(x,y,z) for x,y,z in zip(v,labelembeds,entembs)]
+        strarr += [x['uri'] for x in v]
+        embarr += [x+y for x,y in zip(labelembeds,entembs)]
+        strarr += ['[SEP]']
+        embarr += [500*[-1.0]]
+    for k,v in rellabelcands.items():
+        groundlabel = k
+        groundembed = ft(groundlabel)
+        #print(k, groundembed)
+        labelembeds = ft([x[1] for x in v])
+        relembs = [kgembed(x[0]) for x in v]
+        #[print(x,y,z) for x,y,z in zip(v,labelembeds,relembs)]
+        strarr += [x[0] for x in v]
+        embarr += [x+y for x,y in zip(labelembeds,relembs)]
+        strarr += ['[SEP]']
+        embarr += [500*[-1.0]]
+    return strarr,embarr
+
+
--- scripts/t5inferapi.py
+++ scripts/t5inferapi.py
@@ -1,7 +1,6 @@
-import logging
+import logging,os
 import json
 import sys
-import copy
 import torch.multiprocessing
 from flask import request, Response
 from flask import Flask
@@ -9,6 +8,11 @@ from gevent.pywsgi import WSGIServer
 torch.multiprocessing.set_sharing_strategy('file_system')
 import pandas as pd
 from simpletransformers.t5 import T5Model, T5Args
+from elasticsearch import Elasticsearch
+from sentence_transformers import SentenceTransformer, util
+import Vectoriser
+
+app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
@@ -20,7 +24,54 @@ model_args.use_multiprocessing = False
 model_args.fp16 = False
 
 model = T5Model("t5", "outputs4/checkpoint-11164-epoch-4/", args=model_args)
-app = Flask(__name__)
+
+sentencemodel = SentenceTransformer('all-MiniLM-L6-v2')
+
+reldict = json.loads(open('en.json').read())
+goldrellabels = []
+
+for k,v in reldict.items():
+    goldrellabels.append([k,v])
+
+
+sentence_embeddings = sentencemodel.encode([x[1] for x in goldrellabels], convert_to_tensor=True)
+
+es = Elasticsearch(host='ltcpu1',port=49158)
+print("api ready ...")
+
+def relcands(rellabel):
+    results = []
+    query = rellabel.strip()
+    if not query:
+        return []
+    query_embedding = model.encode(query, convert_to_tensor=True)
+
+    # We use cosine-similarity and torch.topk to find the highest 5 scores
+    cos_scores = util.pytorch_cos_sim(query_embedding, sentence_embeddings)[0]
+    top_results = torch.topk(cos_scores, k=30)
+
+    #print("\n\n======================\n\n")
+    #print("Query:", query)
+    #print("\nTop 5 most similar sentences in corpus:")
+
+    for score, idx in zip(top_results[0], top_results[1]):
+        #print(goldrellabels[idx], "(Score: {:.4f})".format(score))
+        results.append(goldrellabels[idx])
+    return results
+
+def entcands(entlabel):
+    esresults = es.search(index='wikidataentitylabelindex02',body={"query":{"match":{"wikidataLabel":entlabel}}},size=30)
+    results = []
+    try:
+        for res in esresults['hits']['hits']:
+            #print(entlabel, res['_source'])
+            results.append(res['_source'])
+        return results
+    except Exception as err:
+        print(entlabel,err)
+        return results
+
+
 
 @app.route('/ques2labels', methods=['POST'])
 def ques2labels():
@@ -28,7 +79,9 @@ def ques2labels():
     print(d)
     nlquery = d['question']
     preds = model.predict([nlquery])
-    return json.dumps({'nlquery':nlquery,'outputlabels':preds[0]}, indent=4)
+    pred = preds[0]
+    x,y = Vectoriser.vectorise(nlquery,pred)
+    return json.dumps({'nlquery':nlquery,'outputlabels':pred, 'candidatestring': x, 'candidatevectors': y}, indent=4)
 
 if __name__ == '__main__':
     http_server = WSGIServer(('', 2222), app)
